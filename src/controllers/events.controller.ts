e0d93ee4c0902b2554d9f0caaf3b30d1d2d0e28e
Allysa <nuaysa@gmail.com>
2024-12-09 09:24:33 +0700

    events API

--- src/controllers/events.controller.ts
+++ src/controllers/events.controller.ts
@@ -6,6 +6,14 @@ import prisma from "../prisma";
 export class EventsController {
     async getEvents(req: Request, res: Response){
         try{
+            const { search, page = 1, limit = 9 } = req.query;
+            const countEvents = await prisma.user.aggregate({_count: {_all:true}})
+            const totalPage = Math.ceil(countEvents._count._all / +limit)
+            const filter : Prisma.EventWhereInput = {}
+            if(search){
+                filter.title = {contains: search as string, mode: "insensitive"}
+            }
+
             const events = await prisma.event.findMany({
                 select: {
                     id:true,
@@ -27,10 +35,55 @@ export class EventsController {
                             }
                         }
                 }
+            },)
+
+            const result = await prisma.event.findMany({where: filter,
+                orderBy: {id: "asc"},
+                take: +limit,
+                skip: +limit * (+page - 1), 
             })
-            res.status(200).send(events)
+
+            console.log(result)
+            res.status(200).send({events})
         }catch(err){
             console.log(err)
+            res.status(400).send(err)
+        }
+    }
+
+    async getEventSlug (req: Request, res: Response){
+        try{
+            const { slug } = req.params;
+            const event = await prisma.event.findUnique({
+                where: { slug },
+                select: {
+                    id:true,
+                    title: true,
+                    description: true,
+                    category: true,
+                    date: true,
+                    time: true,
+                    location: true,
+                    venue:true,
+                    thumbnail: true,
+                    type: true,
+                    slug: true,
+                }
+            })
+            res.status(200).send({ event })
+        }catch(err){
+            console.log(err);
+            res.status(400).send(err)
+        }
+    }
+
+    async addNewEvent (req: Request, res: Response) {
+        try {
+            const { title, description, category, date, time, location, venue, mapURL, slug, thumbnail, type } = req.body;
+            const newEvent = await prisma.event.create({ data: { title , description, category, date, time, location, venue, mapURL, slug, thumbnail, type } })
+            res.status(200).send("event successfully added !")
+        }catch(err){
+            console.log(err);
             res.status(400).send(err)
         }
     }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors"
+import { EventsRouter } from "./routers/events.router";
 
 const PORT: number = 8000
 
 const app : Application = express()
 app.use(express.json());
 app.use(cors());
+
+const eventsRouter = new EventsRouter()
+
+app.use("/api/events", eventsRouter.getRouter())
 
 app.get("/api", ( req : Request, res: Response ) => {
     res.status(200).send("if this text appear, your API is tunning");
