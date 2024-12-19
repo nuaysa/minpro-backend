3eaca40c5724c8a2710db077f2808788eca803f5
Allysa <nuaysa@gmail.com>
2024-12-19 15:23:34 +0700

    migrate

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -277,7 +277,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
@@ -287,7 +287,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -278,7 +278,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
@@ -288,7 +288,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
