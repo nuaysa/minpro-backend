b011eb851031afda7f6ca331fbedf9f2adf9edb8
nuaysa <66248753+nuaysa@users.noreply.github.com>
2025-01-07 15:14:36 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.BASE_URL_FE_DEV,
+    origin: process.env.BASE_URL_FE,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
